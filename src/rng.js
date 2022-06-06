c9b4d6681cdc488059eb59acf8382aaac5cdd8f1
Rustem Davletov <chicken.zombie7@gmail.com>
2022-06-07 00:47:21 +0300

    New files for brain-even

---
+++ src/rng.js
@@ -0,0 +1,7 @@
+const randomNumber = (min, max) => {
+  const minNumber = Math.ceil(min);
+  const maxNumber = Math.floor(max);
+  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+};
+
+export default randomNumber;
