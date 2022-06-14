6b5c4eaddc24a53dd5c5d24db9c539999cb43fbf
Rustem Davletov <chicken.zombie7@gmail.com>
2022-06-15 01:30:49 +0300

    Add logic for index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import readlineSync from 'readline-sync';
 
-const gameEngine = (gameDescription, gameInfo) => {
-    console.log('Welcome to the Brain Games!');
+const gameEngine = (gameDescription, gameData) => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(gameQuestion);
-}
+  console.log(gameDescription);
 
-for (let i = 0; i < 3; i += 1) {
-    [question, answer] = gameInfo();
-    console.log(`Question: ${gameDescription}`);
+  for (let i = 0; i < 3; i += 1) {
+    const [question, rightAnswer] = gameData();
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
     if (userAnswer === rightAnswer) {
-        console.log('Correct!');
+      console.log('Correct!');
     } else {
-        console.log(`'${answer}' is wrong answer :(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
-        return;
-      };
-    console.log(`Congratulations, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer :(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default gameEngine;
-
