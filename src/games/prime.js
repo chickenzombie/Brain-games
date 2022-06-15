3de9075955d8918d263730078ac3d4d7081b52af
Rustem Davletov <chicken.zombie7@gmail.com>
2022-06-16 01:55:18 +0300

    Add code for brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import randomNumberGenerator from '../rng.js';
+import gameEngine from '../index.js';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isNumberPrime = (number) => {
+  for (let i = 2, max = Math.sqrt(number); i <= max; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return number > 1;
+};
+
+const brainPrime = () => {
+  const randomNumber = randomNumberGenerator(2, 100);
+  const rightAnswer = isNumberPrime(randomNumber) ? 'yes' : 'no';
+  return [randomNumber, rightAnswer];
+};
+
+const startBrainPrime = gameEngine(gameDescription, brainPrime);
+
+export default startBrainPrime;
