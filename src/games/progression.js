ec65373db97869fe006d2b3b2f90619ba2b1bf2d
Rustem Davletov <chicken.zombie7@gmail.com>
2022-06-16 00:46:54 +0300

    Add code for brain-progression

---
+++ src/games/progression.js
@@ -0,0 +1,25 @@
+import randomNumberGenerator from '../rng.js';
+import gameEngine from '../index.js';
+
+const gameDescription = 'What number is missing in the progression?';
+
+const brainProgression = () => {
+  const progression = [];
+  const progressionSize = randomNumberGenerator(5, 10);
+  const progressionStep = randomNumberGenerator(1, 5);
+  let progressionElement = 0;
+
+  for (let i = 0; i < progressionSize; i += 1) {
+    progressionElement += progressionStep;
+    progression.push(progressionElement);
+  }
+  const hiddenNumber = randomNumberGenerator(1, progression.length - 2);
+  const rightAnswer = String(progression[hiddenNumber]);
+  progression[hiddenNumber] = '..';
+  const question = progression.join(' ');
+  return [question, rightAnswer];
+};
+
+const startBrainProgression = gameEngine(gameDescription, brainProgression);
+
+export default startBrainProgression;
